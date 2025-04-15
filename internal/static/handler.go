5986d265dea5099f4e4f8130ed6a1734511c558d
Nikita Galaiko <nikita@galaiko.rocks>
2025-04-15 21:02:52 +0200

    fix url

--- internal/static/handler.go
+++ internal/static/handler.go
@@ -2,6 +2,7 @@ package static
 
 import (
 	"embed"
+	"io/fs"
 	"net/http"
 )
 
@@ -9,5 +10,17 @@ import (
 var staticFS embed.FS
 
 func Handler() http.Handler {
-	return http.FileServer(http.FS(staticFS))
+	fsys := mustFSSub(staticFS, "assets")
+	fileServer := http.FileServer(http.FS(fsys))
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fileServer.ServeHTTP(w, r)
+	})
+}
+
+func mustFSSub(fsys fs.FS, dir string) fs.FS {
+	subFS, err := fs.Sub(fsys, dir)
+	if err != nil {
+		panic(err)
+	}
+	return subFS
 }
