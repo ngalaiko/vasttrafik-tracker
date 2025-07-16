b6b4e0fc7011b878e8e1ecbac60e434aa11bdf62
Nikita Galaiko <nikita@galaiko.rocks>
2025-07-16 20:48:24 +0200

    change tiles

--- packages/web/src/routes/+page.svelte
+++ packages/web/src/routes/+page.svelte
@@ -64,12 +64,11 @@
     const leaflet = await import('leaflet');
     L = leaflet.default;
     map = L.map(mapContainer).setView([currentPosition.latitude, currentPosition.longitude], 13);
-    L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.{ext}', {
-      minZoom: 0,
-      maxZoom: 20,
-      attribution:
-        '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      ext: 'png',
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+      attribution: `&copy;<a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a>,
+		    &copy;<a href="https://carto.com/attributions" target="_blank">CARTO</a>`,
+      subdomains: 'abcd',
+      maxZoom: 16,
     }).addTo(map);
 
     const updateManualPosition = () => {
